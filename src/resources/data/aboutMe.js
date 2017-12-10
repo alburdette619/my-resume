d3d10e64f607d535b36caa3e9da113bdad75fc87
Adam Burdette <alburdet619@gmail.com>
2017-12-10 15:41:21 -0500

    Typos
    Finally reread the copy after a day or so of resting on it and found a few typos.

--- src/resources/data/aboutMe.js
+++ src/resources/data/aboutMe.js
@@ -41,8 +41,8 @@ const aboutMe = {
       job as a manual software tester at Trident Micro Systems.  I'd worked there for about six
       months when they started a new team and I got them to take a chance on me learning to code.  I
       learned C# and WPF while being a junior developer.  I wrote a lot of the front end for a
-      dispatch desktop application that communicated with our radio controllers to telemetry data
-      and provide texting and vocal communications to individuals and groups.
+      dispatch desktop application that communicated with our radio controllers to gather telemetry 
+      data and provide texting and vocal communications to individuals and groups.
     `,
     `
       I continued working on desktop apps in C# through 2015 but noticed that the tide seemed to be
@@ -50,9 +50,10 @@ const aboutMe = {
       There I started with about a year of native Android development before moving on to
       JavaScript.  It seemed like a breath of fresh air after the verbosity of Java.  I haven't
       looked back since.  In late 2016 my direct report, Trevor (@trevorbrindlejs), asked me to 
-      create a project in React Native.  I wound up making small one off app not quite knowing what
-      I was doing but I very much enjoyed the framework.  Together with Trevor, we started migrating
-      the team over to learning React Native and got the go ahead to green field our apps.
+      create a project in React Native.  I wound up making a small one off app not quite knowing 
+      what I was doing but I very much enjoyed the framework.  Together with Trevor, we started 
+      migrating the team over to learning React Native and got the go ahead to green field our
+      current apps.
     `,
     `
       React Native is the first framework in a while that has made me want to get more involved. I
