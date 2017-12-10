69dadc51d0512d8db369ae50a9d1f7de79cc4d22
Adam Burdette <alburdet619@gmail.com>
2017-12-10 01:16:37 -0500

    Finialized Education screen
    Added a logo for NCSU and filled out the education screen with a few points.

--- src/components/education.js
+++ src/components/education.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, ScrollView } from 'react-native';
+
+const ncsuLogo = require('../resources/images/ncsu_logo.png');
+
+const { width } = Dimensions.get('window');
+const containerMargin = 10;
+const imageWidthOffset = 2 * containerMargin;
+const imageWidth = width - imageWidthOffset;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: containerMargin,
+    flex: 1,
+  },
+  ncsu: {
+    width: imageWidth,
+    height: 0.62 * imageWidth,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 5,
+  },
+});
+
+const edPoints =
+[
+  'Bachelor of Science in Engineering with a concentration in Mechatronics',
+  'Attended 2003 - 2007',
+  'Worked with a team to develop a solar water heater that tracked the sun.',
+];
 
 const EducationView = () => (
-  <View>
-    <Text>Education</Text>
-  </View>
+  <ScrollView style={styles.container}>
+    <Image source={ncsuLogo} style={styles.ncsu} />
+    {
+      edPoints.map(point => <Text key={point} style={styles.text} >{`\u2023 ${point}`}</Text>)
+    }
+
+  </ScrollView>
 );
 
 export default EducationView;
